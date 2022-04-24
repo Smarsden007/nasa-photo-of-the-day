20864dd7c9a6cabe51044b7d53341d8eba5f7fe7
Smarsden007 <seannmarsden@gmail.com>
2022-04-24 00:46:31 -0700

    added header and some styling to text boxes

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -4,7 +4,9 @@ import React from "react";
 const NasaPhoto = (props) => {
     return (
         <div className="nasa-photo-wrapper">
+            <header>
             <h3>{props.photo.title}</h3>
+            </header>
             <p>{props.photo.date}</p>
             <img src={props.photo.hdurl} />
             <p>{props.photo.explanation}</p>
